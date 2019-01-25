5ca771bd0c801bcaabcce7d0eca1773f17788c23
danielarnason <danielarnason85@gmail.com>
2019-01-25 14:29:55 +0100

    nu i functions

--- scraper.py
+++ scraper.py
@@ -2,25 +2,33 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome('chromedriver.exe')
-driver.get('https://statistik.uni-c.dk/instregvisning/oversigt.aspx')
+def get_skoledata(kommune):
+    driver = webdriver.Chrome('chromedriver.exe')
+    driver.get('https://statistik.uni-c.dk/instregvisning/oversigt.aspx')
 
-grundskoler = driver.find_element_by_id('ContentPlaceHolder1_TreeViewInstt1')
-grundskoler.click()
+    grundskoler = driver.find_element_by_id('ContentPlaceHolder1_TreeViewInstt1')
+    grundskoler.click()
 
-folkeskoler = driver.find_element_by_id('ContentPlaceHolder1_TreeViewInstt4')
-folkeskoler.click()
+    folkeskoler = driver.find_element_by_id('ContentPlaceHolder1_TreeViewInstt4')
+    folkeskoler.click()
 
-egekom = driver.find_element_by_id('ContentPlaceHolder1_DropDownListKommune').send_keys('Egedal')
+    driver.find_element_by_id('ContentPlaceHolder1_DropDownListKommune').send_keys(kommune)
 
-trs = driver.find_elements(By.TAG_NAME, 'tr')
+    trs = driver.find_elements(By.TAG_NAME, 'tr')
 
-skoler = [tr.text.split(',  ') for tr in trs]
+    # driver.close()
 
-df = pd.DataFrame(skoler, columns=['Skolenavn', 'Adresse', 'Postnrby', 'Tlf', 'Mail'])
-df['Tlf'] = [x.strip('tlf: ') for x in df['Tlf']]
-df['Mail'] = [x.strip('e-mail: ') for x in df['Mail']]
-df['Skolenavn'] = [' '.join(x.split(' ')[:-1]) for x in df['Skolenavn']]
-print(df)
+    return [tr.text.split(',  ') for tr in trs]
 
-driver.close()
+def create_df(skoledata):
+    df = pd.DataFrame(skoledata, columns=['Skolenavn', 'Adresse', 'Postnrby', 'Tlf', 'Mail'])
+    df['Tlf'] = [x.strip('tlf: ') for x in df['Tlf']]
+    df['Mail'] = [x.strip('e-mail: ') for x in df['Mail']]
+    df['Skolenavn'] = [' '.join(x.split(' ')[:-1]) for x in df['Skolenavn']]
+
+    return df
+
+if __name__ == "__main__":
+    skoler = get_skoledata('Egedal')
+    df = create_df(skoler)
+    print(df)
